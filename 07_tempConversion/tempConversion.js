e498cfc3b792440518023fbc1f7ca7f36148b411
Laurens <laurens.krom@gmail.com>
2024-08-02 03:50:44 +0200

    passed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsius = (fahr - 32) * (5/9);  
+  let rounded = Math.round(celsius * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahrenheit = (9/5 * cels) + 32;
+  let rounded = Math.round(fahrenheit * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
