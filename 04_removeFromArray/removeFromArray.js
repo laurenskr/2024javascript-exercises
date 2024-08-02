787eef4bddd1597047ad9368957943b0c8c495fa
Laurens <laurens.krom@gmail.com>
2024-08-02 03:19:21 +0200

    passed all removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    let outputArray = [];
+    inputArray.forEach((element) => {
+        if (!args.includes(element)) {
+            outputArray.push(element);
+        }
+    });
+    return outputArray;
 };
 
 // Do not edit below this line
