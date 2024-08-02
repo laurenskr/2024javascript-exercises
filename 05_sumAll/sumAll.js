6d2324aab83fc485dd2165e36d29624311c90c51
Laurens <laurens.krom@gmail.com>
2024-08-02 03:36:57 +0200

    passed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR"
+    }
+    let small = start < end ? start : end;
+    let big = start < end ? end : start;
+    
+    if (small < 0) {
+        return "ERROR"
+    }
+    let sum = 0;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
